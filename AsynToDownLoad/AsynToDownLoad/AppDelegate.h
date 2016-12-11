33812b0659eacec4c0f683f25ed3feff7abce1d3
YesterDayIsDie <hukun1987@126.com>
2016-12-11 17:09:48 +0800

    01.工程初始化完成

---
+++ AsynToDownLoad/AsynToDownLoad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AsynToDownLoad
+//
+//  Created by 胡坤 on 2016/12/11.
+//  Copyright © 2016年 hukun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AsynToDownLoad/AsynToDownLoad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AsynToDownLoad
+//
+//  Created by 胡坤 on 2016/12/11.
+//  Copyright © 2016年 hukun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
