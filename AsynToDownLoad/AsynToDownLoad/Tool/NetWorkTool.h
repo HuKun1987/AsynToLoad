54a911deeaec445960a464f553a4bc97a184f234
YesterDayIsDie <hukun1987@126.com>
2016-12-11 17:17:10 +0800

    02.导入AFN第三方工具完成

---
+++ AsynToDownLoad/AsynToDownLoad/Tool/NetWorkTool.h
@@ -0,0 +1,13 @@
+//
+//  NetWorkTool.h
+//  AsynToDownLoad
+//
+//  Created by 胡坤 on 2016/12/11.
+//  Copyright © 2016年 hukun. All rights reserved.
+//
+
+#import "AFURLSessionManager.h"
+
+@interface NetWorkTool : AFURLSessionManager
+
+@end
---
+++ AsynToDownLoad/AsynToDownLoad/Tool/NetWorkTool.m
@@ -0,0 +1,13 @@
+//
+//  NetWorkTool.m
+//  AsynToDownLoad
+//
+//  Created by 胡坤 on 2016/12/11.
+//  Copyright © 2016年 hukun. All rights reserved.
+//
+
+#import "NetWorkTool.h"
+
+@implementation NetWorkTool
+
+@end
